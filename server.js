3b734eb1c1625447ce0efdedc365559476ff4546
Ilja Winokurow <ilja_winokurow@yahoo.com>
2022-10-31 20:21:34 +0100

    Fix upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/food-manager'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/food-manager/index.html'));});
+app.use(express.static(__dirname + '/dist/foodmanager'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/foodmanager/index.html'));});
 app.listen(process.env.PORT || 8080);
