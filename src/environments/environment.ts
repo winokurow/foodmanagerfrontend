d8819ff95034c7b5a7050ccbe9bf05823453698b
winokurow <ilja_winokurow@yahoo.com>
2025-05-11 06:28:23 +0200

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080',
-  supabaseUrl: 'https://rvyvbmnpjyfwipezvbjz.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2eXZibW5wanlmd2lwZXp2Ymp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg4MTgwMTQsImV4cCI6MjA0NDM5NDAxNH0.ZtFj7_Xu0bzRI98uFx_AmhDuZ6YpIks7vojhj2C19rs',
-};
